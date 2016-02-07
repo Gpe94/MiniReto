8be4ff3ea59fb2b38071bba4cb4366049be9a3fc
Zilva <zilva@Zilvas-Mac-Pro.local>
2016-02-06 23:12:38 -0800

    Initial Commit

---
+++ Mini-Reto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mini-Reto
+//
+//  Created by Zilva on 06/02/16.
+//  Copyright (c) 2016 Zilva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
